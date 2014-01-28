e5436eea4bded3cb796bd5c2569a2294e615b90f
Michael Zhou <mzhou@cse.unsw.edu.au>
2014-01-28 13:43:44 +0000

    Platform and privileged
    Change-Id: I091dd6a0f50ea67d8f7513f77751d5ed4b9ec214

--- Superuser/Android.mk
+++ Superuser/Android.mk
@@ -3,6 +3,8 @@ include $(CLEAR_VARS)
 
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 LOCAL_PACKAGE_NAME := Superuser
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
 LOCAL_SRC_FILES := $(call all-java-files-under,src) $(call all-java-files-under,../../Widgets/Widgets/src)
 
 LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
